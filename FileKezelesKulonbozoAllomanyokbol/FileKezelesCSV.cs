2571bb97a9789259022a1b4b716f53cf57601b41
pepe1973 <bodrogipr@gmail.com>
2023-09-18 06:38:48 +0200

    Add project files.

---
+++ FileKezelesKulonbozoAllomanyokbol/FileKezelesCSV.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileKezelesKulonbozoAllomanyokbol
+{
+    public class FileKezelesCSV
+    {
+        public static List<string[]> lista = new List<string[]>();
+        
+        public static List<string[]> FilebolOlvas(string fileNev)
+        {
+            string elsoSor = "";
+
+            using (StreamReader sr = new StreamReader(fileNev, Encoding.UTF8))
+            {
+                elsoSor = sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    string[] tomb = sor.Split(';');
+                    lista.Add(tomb);
+                }
+            }
+
+            return lista;
+        }
+
+        public static int ElemszamSzamolas()
+        {
+            int elemszam = lista.Count;
+
+            return elemszam;
+        }
+    }
+}
